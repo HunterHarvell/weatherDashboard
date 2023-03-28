6b5822fe7b8c1b9967c87ba94e911150d904fcab
HunterHarvell <hunterharvell95@gmail.com>
2023-03-28 15:04:11 -0500

    parse weather data

--- assets/script.js
+++ assets/script.js
@@ -25,5 +25,20 @@ function startPage() {
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
         axios.get(queryURL)
             .then(function (response) {
+                currentWeather.classList.remove("d-none");
+
+                // Parse response to display current weather
+                const currentDate = new Date(response.data.dt * 1000);
+                const day = currentDate.getDate();
+                const month = currentDate.getMonth() + 1;
+                const year = currentDate.getFullYear();
+                currentCity.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+                let weatherPic = response.data.weather[0].icon;
+                weatherSymbol.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+                weatherSymbol.setAttribute("alt", response.data.weather[0].description);
+                temp.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+                humidity.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+                windSpeed.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+
                 
-            })
+            });
